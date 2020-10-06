52b03ed4ff11e14481aa6c024d841b95453c55d6
vagrant <shadmerhi@gmail.com>
2020-10-06 07:37:17 +0000

    Retrieved '.counter' innerHTML to allow for modification

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   let counter = 0;
   $("#tweet-text").keyup(function(event) {
-    console.log(this.value.length);
+    // console.log('char count', this.value.length);
+    let thing = $(this).parents('.form-flex').children('div:last').children('.counter');
+    console.log(thing[0].innerHTML);
   });
 });
 
