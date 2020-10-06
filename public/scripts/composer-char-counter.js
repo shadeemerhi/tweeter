bd8c476a5ea0ef07719a062a7ddfd22430114b48
vagrant <shadmerhi@gmail.com>
2020-10-06 08:04:30 +0000

    Char counter fully functional - changes to red when negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
-  let counter = 0;
   $("#tweet-text").keyup(function(event) {
     // console.log('char count', this.value.length);
+
+    // Each time a character is entered, we retrieve the .counter output element from the DOM
     const counterElement = $(this).parents('.form-flex').children('div:last').children('.counter');
-    let charCount = parseInt(counterElement[0].innerHTML) - 1;
-    charCount = charCount.toString();
-    counterElement[0].innerHTML = charCount;
+
+    // Updating the value of the character count using the length of the input text
+    const charCount = 140 - this.value.length;
+
+    // If the character count is below 0, the color is changed to red, otherwise it should be it's normal color
+    if (charCount < 0) {
+      counterElement.css('color', 'red');
+    } else {
+      counterElement.css('color', '#545149');
+    }
+
+    // Updating the innterHTML of the counter to display to the user
+    counterElement[0].innerHTML = charCount.toString();
+
+
   });
 });
 
