fb35f20a828091e081accb3f9e9f3f1866a6fb55
vagrant <shadmerhi@gmail.com>
2020-10-07 02:16:55 +0000

    Client JS now renders single tweet using hard coded object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,63 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+  const createTweetElement = function(tweet) {
+    let $tweet = 
+      `<article>
+          <div class="tweet-container">
+          <header>
+            <div class="img-name">
+              <img class="profile-pic" src=${tweet.user.avatars}>
+              <p>${tweet.user.name}</p>
+            </div>
+            <p class="handle">${tweet.user.handle}</p>
+          </header>
+          <div class="tweet-content">
+            <p>${tweet.content.text}</p>
+          </div>
+          <footer>
+            <p>${tweet['created_at']}</p>
+            <div class="eng-icons">
+              <i class="fas fa-flag"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-heart"></i>
+            </div>
+          </footer>
+        </div>
+      </article>`;
+
+      // console.log($tweet);
+      return $tweet;
+  }
+
+
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": '10 Minutes Ago'
+  }
+
+  const $tweet = createTweetElement(tweetData);
+
+  $('.tweet-feed').append($tweet);
+
+});
+
+
 const renderTweets = function(tweets) {
+
   
 }
 
-const createTweetElement = function(tweet) {
 
-  let $tweet = '';
-
-  return $tweet;
-
-}
-
-
-// A single tweet object
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-// Array of tweet objects
 const data = [
   {
     "user": {
@@ -54,4 +84,4 @@ const data = [
     },
     "created_at": 1461113959088
   }
-]
+// ]
