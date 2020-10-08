de2ebc21f876937adb45fc29075e12b0aeb1459e
vagrant <shadmerhi@gmail.com>
2020-10-08 04:37:55 +0000

    Reponsive design implemented - minor bugs exist

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,14 +1,8 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-
 $(document).ready(function() {
 
+  // Creates new HTML element for each tweet as they are submitted by the user and returns said element to be rendered on the page
   const createTweetElement = function(tweet) {
-    let $tweet = 
+    let $tweet =
       `<article>
           <div class="tweet-container">
           <header>
@@ -31,48 +25,39 @@ $(document).ready(function() {
           </footer>
         </div>
       </article>`;
+    return $tweet;
+  };
 
-      return $tweet;
-  }
-
+  // Calls createTweetElement for new tweets and appends each one to the feed
   const renderTweets = function(tweets) {
-
     $('.tweet-feed').empty();
     tweets.forEach(tweet => {
       const $tweet = createTweetElement(tweet);
       $('.tweet-feed').append($tweet);
     });
-  }
+  };
 
-const escape = function(str) {
-  let div = document.createElement('div');
+  // XSS protection - converts any malicious input that could alter the code a string
+  const escape = function(str) {
+    let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-}
+  };
 
-  // Creating the POST request using data from the form submit
+  // Adding event listener to the input form
   $('#submit-tweet').submit(function(event) {
     event.preventDefault();
 
-
-    // Form validation --> empty input, or input over 140 characters
+    // Storing the counter in a variable to allow for resetting to 140 after tweet is submitted
     const counterElement = $('#tweet-text').parents('.form-flex').children('div:last').children('.counter');
+
+    // Form validation - checking if input is empty or if over 140 characters - if tweet OK, AJAX POST request to server
     const charCount = $('#tweet-text').val().length;
-
-
-    // Cross-site scripting - not fully working
-    // const textFromUser = $('#tweet-text').val();
-    // console.log('text from user', textFromUser);
-    // const testThing = $("<div>").text(textFromUser);
-    // console.log('testthing', testThing[0].innerHTML);
-    // $('#tweet-text').innerHTML = testThing[0].innerHTML;
-
-
     if (charCount > 140) {
-      $('#null-error').hide();
+      $('#null-error').slideUp('fast');
       $('#char-error').slideDown('fast');
     } else if (charCount === 0) {
-      $('#char-error').hide();
+      $('#char-error').slideUp('fast');
       $('#null-error').slideDown('fast');
     } else {
       $.ajax({
@@ -80,32 +65,31 @@ const escape = function(str) {
         type: 'POST',
         data: $(this).serialize()
       }).then(() => {
+        // Resets the character counter on the page back to 140, and hides any errors that may be visible
         counterElement[0].innerHTML = '140';
-        $('#char-error').slideUp('fast');
         $('#null-error').slideUp('fast');
+        $('#char-error').slideUp('fast');
         $('#tweet-text').val('');
-        return loadTweets()});
+        return loadTweets();
+      });
     }
   });
 
+  // AJAX GET request to retreive the tweet database
   const loadTweets = function() {
     $.ajax({
       url: '/tweets',
       type: 'GET',
       dataType: 'json',
     }).then(data => {
-      // Do I need to return line below?
       renderTweets(data);
     });
-  }
-  
-  loadTweets();
+  };
+
+  // Hiding the error messages on page load - will be shown as needed on form submission (see above)
   $('#null-error').hide();
   $('#char-error').hide();
 
+  // Loading the existing tweets from the database
+  loadTweets();
 });
-
-
-
-
-
