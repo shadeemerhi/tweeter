176f084b843b97ff73a16b8b5197e29f3e245a8d
vagrant <shadmerhi@gmail.com>
2020-10-09 04:10:56 +0000

    Clean up and comment additions

--- public/scripts/scroll-toggle.js
+++ public/scripts/scroll-toggle.js
@@ -7,23 +7,24 @@ $(document).ready(function() {
 
   // When the 'Write a new tweet' button is clicked, the Compose Tweet section toggles up/down
   $('.new-tweet').click(function() {
-    $('html,body').animate({ scrollTop: $('.head-feed').offset().top-150 }, 1000);
 
+    // $('html,body').animate({ scrollTop: $('.head-feed').offset().top-150 }, 1000);
     $('#compose').toggle('slow');
-
     $('#tweet-text').focus();
 
-
-
-    // $('#compose').slideToggle('slow');
   });
 
+  // Toggles appropriate classes on various elements on scroll past the height of the nav
   $(function () {
     $(document).scroll(function () {
+
+      // Creating variables using elements that have classes that toggle
       const $nav = $(".sticky");
       const $tweetButton = $('.new-tweet');
       const $logo = $('.logo');
       const $scrollButton = $('.scroll-button');
+
+      // Toggling element classes based on user scroll behavior
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
       $tweetButton.toggleClass('hidden', $(this).scrollTop() > $nav.height());
       $logo.toggleClass('hidden', $(this).scrollTop() > $nav.height());
@@ -31,12 +32,14 @@ $(document).ready(function() {
     });
   });
 
+  // Scroll-to-top button appears when user scrolls down - when clickes, scrolls to top and opens tweet composer
   $('.scroll-button').click(function() {
-    $('html,body').animate({ scrollTop: $('.head-feed').offset().top-150 }, 500);
-    if($('#compose').is(':hidden')) {
+    $('html,body').animate({ scrollTop: $('.head-feed').offset().top - 150 }, 500);
+    if ($('#compose').is(':hidden')) {
       $('#compose').slideDown('slow');
       $('#tweet-text').focus();
-      // $(this).hide();
+    } else {
+      $('#tweet-text').focus();
     }
   });
 
