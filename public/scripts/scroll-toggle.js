378e29ef9a2b1126a8767dc1fd3fd21ef1c242c6
vagrant <shadmerhi@gmail.com>
2020-10-09 03:57:50 +0000

    Scroll-to-top button added - contains minor bugs

--- public/scripts/scroll-toggle.js
+++ public/scripts/scroll-toggle.js
@@ -2,16 +2,14 @@ $(document).ready(function() {
 
   // Adding scroll functionality so logo click brings back to top
   $('.logo').click(function() {
-    $('html,body').animate({ scrollTop: $('.head-feed').offset().top-150 }, 1000);
+    $('html,body').animate({ scrollTop: $('.head-feed').offset().top-150 }, 500);
   });
 
   // When the 'Write a new tweet' button is clicked, the Compose Tweet section toggles up/down
   $('.new-tweet').click(function() {
     $('html,body').animate({ scrollTop: $('.head-feed').offset().top-150 }, 1000);
 
-    $('#compose').toggle('slow', function() {
-
-    });
+    $('#compose').toggle('slow');
 
     $('#tweet-text').focus();
 
@@ -25,10 +23,21 @@ $(document).ready(function() {
       const $nav = $(".sticky");
       const $tweetButton = $('.new-tweet');
       const $logo = $('.logo');
+      const $scrollButton = $('.scroll-button');
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
       $tweetButton.toggleClass('hidden', $(this).scrollTop() > $nav.height());
-      $logo.toggleClass('color-toggle', $(this).scrollTop() > $nav.height());
+      $logo.toggleClass('hidden', $(this).scrollTop() > $nav.height());
+      $scrollButton.toggleClass('hidden', $(this).scrollTop() < $nav.height());
     });
+  });
+
+  $('.scroll-button').click(function() {
+    $('html,body').animate({ scrollTop: $('.head-feed').offset().top-150 }, 500);
+    if($('#compose').is(':hidden')) {
+      $('#compose').slideDown('slow');
+      $('#tweet-text').focus();
+      // $(this).hide();
+    }
   });
 
   // Hiding the Compose Tweet section on page load
