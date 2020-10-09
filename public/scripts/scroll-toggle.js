e7a3cb5702a296f6535ba4705f79fff0c497246f
vagrant <shadmerhi@gmail.com>
2020-10-09 03:10:31 +0000

    'Write tweet' button disappears when user scrolls down

--- public/scripts/scroll-toggle.js
+++ public/scripts/scroll-toggle.js
@@ -8,8 +8,25 @@ $(document).ready(function() {
   // When the 'Write a new tweet' button is clicked, the Compose Tweet section toggles up/down
   $('.new-tweet').click(function() {
     $('html,body').animate({ scrollTop: $('.head-feed').offset().top-150 }, 1000);
-    $('#compose').slideToggle('slow');
+
+    $('#compose').toggle('slow', function() {
+
+    });
+
     $('#tweet-text').focus();
+
+
+
+    // $('#compose').slideToggle('slow');
+  });
+
+  $(function () {
+    $(document).scroll(function () {
+      const $nav = $(".sticky");
+      const $tweetButton = $('.new-tweet');
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      $tweetButton.toggleClass('hidden', $(this).scrollTop() > $nav.height());
+    });
   });
 
   // Hiding the Compose Tweet section on page load
